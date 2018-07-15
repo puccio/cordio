edfa7d983228b50a024246ebb47ec9fb1b85e827
pucciogiacalone <pucciogiacalone@gmail.com>
2018-07-15 23:07:30 +0200

    First look

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+    // init Masonry
+    var $grid = $('.grid').masonry({
+        itemSelector: '.grid-item',
+        columnWidth: 220,
+        gutter: 20
+    });
+});
+
