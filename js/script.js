d00ce1aa312c4cdd917c4cd87554050d53805317
pucciogiacalone <pucciogiacalone@gmail.com>
2018-07-18 23:53:09 +0200

    fix gallery foreach breakpoint in homepage

--- js/script.js
+++ js/script.js
@@ -2,8 +2,36 @@ $( document ).ready(function() {
     // init Masonry
     var $grid = $('.grid').masonry({
         itemSelector: '.grid-item',
-        columnWidth: 220,
-        gutter: 20
+        gutter: '.gutter-sizer'
+        // columnWidth: 25%,
+        // gutter: 20
     });
 });
 
+// $(window).on('resize', function (event) {
+//     var $window = $(window);
+  
+//     if ($window.width() <= 320) {
+//         var $masonryTarget = $('.grid');
+        
+//         $hasMasonry = $masonryTarget.data('masonry') ? true : false;
+
+//         if ($masonryTarget.length > 0 && $hasMasonry) {
+//                 // Destroy masonry if exists.
+//                 $masonryTarget.masonry('destroy');
+//                 alert(0);
+//                 var $grid = $('.grid').masonry({
+//                     itemSelector: '.grid-item',
+//                     columnWidth: 280,
+//                 });
+//         }
+//     } else {
+//       // Enable all masonry instances.
+//       alert(1);
+//         var $grid = $('.grid').masonry({
+//             itemSelector: '.grid-item',
+//             columnWidth: 220,
+//             gutter: 20
+//         });
+//     }
+//   });
