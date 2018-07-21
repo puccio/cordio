64b95fe2e9cab11c94898cac8d3b433d646bfd07
pucciogiacalone <pucciogiacalone@gmail.com>
2018-07-21 23:07:38 +0200

    add filter logic on gallery page; fixed mansory issue with load image

--- js/script.js
+++ js/script.js
@@ -1,44 +1,114 @@
 $( document ).ready(function() {
+
     // init Masonry
-    var $grid = $('.grid').masonry({
-        itemSelector: '.grid-item',
-        gutter: '.gutter-sizer'
-        // columnWidth: 25%,
-        // gutter: 20
+    $grid = $('.grid').imagesLoaded( function() {
+        $grid.masonry({
+            itemSelector: '.grid-item',
+            gutter: '.gutter-sizer'
+            // columnWidth: 25%,
+            // gutter: 20
+        }); 
+      });
+
+    // $(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    //     event.preventDefault();
+    //     $(this).ekkoLightbox({
+    //         alwaysShowClose: true
+    //     });
+    // });
+
+    $("#gallery-page .anchors .anchor").click(function() {
+
+        // active anchor
+        var activeAnchor = $(this).attr("href");
+
+        // clicked anchor
+        var clickedAnchor = $("#gallery-page .anchors .anchor.active").attr("href");
+
+        // if anchor clicked is different to the current
+        if(activeAnchor !== clickedAnchor) {
+
+            // all images of the gallery
+            var oldImages = $('.grid-item');
+
+            // disabled all anchors link filters
+            $("#gallery-page .anchors .anchor").removeClass("active");
+
+            // set active anchor clicked
+            $(this).addClass("active");
+
+            // make an AJAX GET: request new images of category clicked as JSON
+            allImages = {
+                            "images":   
+                                [
+                                    {
+                                        "src": "img/gallery/IN_59_Periferia-romana.jpg",
+                                        "title": "Periferia romana",
+                                        "category": "affreschi",
+                                        "year": "1959"
+                                    },
+                                    {
+                                        "src": "img/gallery/IN_59_Periferia-romana.jpg",
+                                        "title": "Periferia romana",
+                                        "category": "affreschi",
+                                        "year": "1959"
+                                    },
+                                    {
+                                        "src": "img/gallery/IN_59_Periferia-romana.jpg",
+                                        "title": "Periferia romana",
+                                        "category": "affreschi",
+                                        "year": "1959"
+                                    },
+                                    {
+                                        "src": "img/gallery/IN_59_Periferia-romana.jpg",
+                                        "title": "Periferia romana",
+                                        "category": "affreschi",
+                                        "year": "1959"
+                                    },
+                                    {
+                                        "src": "img/gallery/IN_59_Periferia-romana.jpg",
+                                        "title": "Periferia romana",
+                                        "category": "affreschi",
+                                        "year": "1959"
+                                    },
+                                    {
+                                        "src": "img/gallery/IN_59_Periferia-romana.jpg",
+                                        "title": "Periferia romana",
+                                        "category": "affreschi",
+                                        "year": "1959"
+                                    },
+                                    {
+                                        "src": "img/gallery/IN_59_Periferia-romana.jpg",
+                                        "title": "Periferia romana",
+                                        "category": "affreschi",
+                                        "year": "1959"
+                                    }
+                                    ,
+                                    {
+                                        "src": "img/gallery/IN_59_Periferia-romana.jpg",
+                                        "title": "Periferia romana",
+                                        "category": "affreschi",
+                                        "year": "1959"
+                                    }
+                                ]
+                        };
+
+            $.each(allImages.images, function(i, image) {
+                var newImages = $('<div class="grid-item painting-container" name="#affreschi"><div class="row"><a href="' + image.src + '" data-toggle="lightbox" data-gallery="example-gallery"><img class="col-md-12 painting-container-img" src="' + image.src + '"></a><div class="col-md-12 painting-container-title">' + image.title + '</div><div class="col-md-12 painting-container-year">' + image.year + '</div></div></div>');
+            
+                // from string to jquery object
+                var $newImages = $(newImages);
+
+                // appending new images
+                $grid.append(newImages).masonry('appended', newImages);
+            })
+
+            // removing old images
+            $grid.masonry('remove', $(oldImages)).masonry('layout');
+              
+        }
+
+        return false;
     });
 
-    $(document).on('click', '[data-toggle="lightbox"]', function(event) {
-        event.preventDefault();
-        $(this).ekkoLightbox({
-            alwaysShowClose: true
-        });
-    });
 });
-
-// $(window).on('resize', function (event) {
-//     var $window = $(window);
-  
-//     if ($window.width() <= 320) {
-//         var $masonryTarget = $('.grid');
-        
-//         $hasMasonry = $masonryTarget.data('masonry') ? true : false;
-
-//         if ($masonryTarget.length > 0 && $hasMasonry) {
-//                 // Destroy masonry if exists.
-//                 $masonryTarget.masonry('destroy');
-//                 alert(0);
-//                 var $grid = $('.grid').masonry({
-//                     itemSelector: '.grid-item',
-//                     columnWidth: 280,
-//                 });
-//         }
-//     } else {
-//       // Enable all masonry instances.
-//       alert(1);
-//         var $grid = $('.grid').masonry({
-//             itemSelector: '.grid-item',
-//             columnWidth: 220,
-//             gutter: 20
-//         });
-//     }
-//   });
