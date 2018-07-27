a538463bf23554ef26399d37c3e9854513cd4b54
pucciogiacalone <pucciogiacalone@gmail.com>
2018-07-27 12:35:39 +0200

    removed loadmore button when filter clicked and there are no others image available

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@ $( document ).ready(function() {
 
     // qui lo dichiaro statico
     allImages = {
-        "numberOfimagesToShow": 8,                  // definisce quante immagini mostrare
-        "numberOfimagesToShowOnLoadmore": 4,        // definisce quante immagini mostrare al loadmore
+        "numberOfimagesToShow": 8,                  // definisce quante immagini mostrare al caricamento della pagina
+        "numberOfimagesToShowOnLoadmore": 8,        // definisce quante immagini mostrare al loadmore
         "images":   
             [
                 {
@@ -295,22 +295,6 @@ $( document ).ready(function() {
                 }
                 ,
                 {
-                    "src_low": "img/gallery/IN_59_Periferia-romana.jpg",
-                    "src_high": "img/gallery/IN_59_Periferia-romana.jpg",
-                    "title": "Periferia romana",
-                    "category": "#affreschi",
-                    "year": "1959",
-                    "description": ""
-                },
-                {
-                    "src_low": "img/gallery/AF_94_Primavera.jpg",
-                    "src_high": "img/gallery/AF_94_Primavera.jpg",
-                    "title": "Primavera",
-                    "category": "#affreschi",
-                    "year": "1959",
-                    "description": ""
-                },
-                {
                     "src_low": "img/gallery/AF_94_Uccelli-nel-bosco.jpg",
                     "src_high": "img/gallery/AF_94_Uccelli-nel-bosco.jpg",
                     "title": "Uccelli nel bosco",
@@ -360,22 +344,6 @@ $( document ).ready(function() {
                     "description": ""
                 }
                 ,
-                {
-                    "src_low": "img/gallery/IN_59_Periferia-romana.jpg",
-                    "src_high": "img/gallery/IN_59_Periferia-romana.jpg",
-                    "title": "Periferia romana",
-                    "category": "#affreschi",
-                    "year": "1959",
-                    "description": ""
-                },
-                {
-                    "src_low": "img/gallery/AF_94_Primavera.jpg",
-                    "src_high": "img/gallery/AF_94_Primavera.jpg",
-                    "title": "Primavera",
-                    "category": "#affreschi",
-                    "year": "1959",
-                    "description": ""
-                },
                 {
                     "src_low": "img/gallery/AF_94_Uccelli-nel-bosco.jpg",
                     "src_high": "img/gallery/AF_94_Uccelli-nel-bosco.jpg",
@@ -494,32 +462,6 @@ $( document ).ready(function() {
         return false;
     });
 
-    // $("#gallery-page .showmore").on('click', function() {
-
-    //     var allImagesTemp = allImages.images;
-
-    //     for(var i = numberOfImagesShowed; i < allImagesTemp.length; i++) {
-            
-    //         var newImages = $('<div class="grid-item painting-container"><div class="row"><a href="' + allImagesTemp[i].src_high + '" data-toggle="lightbox" data-gallery="example-gallery"><img class="col-md-12 painting-container-img" src="' + allImagesTemp[i].src_low + '"></a><div class="col-md-12 painting-container-title">' + allImagesTemp[i].title + '</div><div class="col-md-12 painting-container-year">' + allImagesTemp[i].year + '</div></div></div>');
-        
-    //         // from string to jquery object
-    //         var $newImages = $(newImages);
-
-    //         // appending new images
-    //         $grid.append(newImages).masonry('appended', newImages);
-
-    //         // update the field
-    //         numberOfImagesShowed++;
-    //     }
-
-    //     // if all images are showed
-    //     if( numberOfImagesShowed == allImages.images.length ) {
-    //         $('.showmore').hide();
-    //     }
-
-    //     return false;
-    // });
-
     $(document).on('click', '[data-toggle="lightbox"]', function(event) {
         event.preventDefault();
 
@@ -553,7 +495,7 @@ $( document ).ready(function() {
             // set active anchor clicked
             $(this).addClass("active");
 
-            // prendo le immagini della categoria cliccata
+            // tutte le immagini della categoria cliccata
             var allImagesTemp = getImagesFromCategory(clickedAnchor);
 
             var numberOfimagesToShow = allImages.numberOfimagesToShow;
@@ -561,26 +503,27 @@ $( document ).ready(function() {
             numberOfImagesShowed = 0;
 
             for(var i = 0; i < allImagesTemp.length && i < numberOfimagesToShow; i++) {
-                if( allImagesTemp[i].category === clickedAnchor || clickedAnchor === "#all" ) {
 
-                    var newImages = $('<div class="grid-item painting-container"><div class="row"><a href="' + allImagesTemp[i].src_high + '" data-toggle="lightbox" data-gallery="example-gallery"><img class="col-md-12 painting-container-img" src="' + allImagesTemp[i].src_low + '"></a><div class="col-md-12 painting-container-title">' + allImagesTemp[i].title + '</div><div class="col-md-12 painting-container-year">' + allImagesTemp[i].year + '</div></div></div>');
-                
-                    // from string to jquery object
-                    var $newImages = $(newImages);
+                var newImages = $('<div class="grid-item painting-container"><div class="row"><a href="' + allImagesTemp[i].src_high + '" data-toggle="lightbox" data-gallery="example-gallery"><img class="col-md-12 painting-container-img" src="' + allImagesTemp[i].src_low + '"></a><div class="col-md-12 painting-container-title">' + allImagesTemp[i].title + '</div><div class="col-md-12 painting-container-year">' + allImagesTemp[i].year + '</div></div></div>');
+            
+                // from string to jquery object
+                var $newImages = $(newImages);
 
-                    // appending new images
-                    $grid.append(newImages).masonry('appended', newImages);
+                // appending new images
+                $grid.append(newImages).masonry('appended', newImages);
 
-                    // update the field
-                    numberOfImagesShowed++;
-                }
+                // update the field
+                numberOfImagesShowed++;
             }
 
             // removing old images
             $grid.masonry('remove', $(oldImages)).masonry('layout');
-            
 
-            // rimuovere il tasto mostra altro se non ci sono altri risultati
+            // if all images are showed
+            if( numberOfImagesShowed == allImagesTemp.length ) {
+                $('.showmore').hide();
+            }
+
         }
 
         return false;
